cf8086a5949f8855acc7ab3aa785b72ac4b25311
Gaurav Manek <gaurav@gauravmanek.com>
2026-02-17 12:22:35 +0000

    Create the .log folder if it doesn't exist.

--- run.sh
+++ run.sh
@@ -4,4 +4,5 @@ typst compile --font-path ./fonts "labelgen.typ"
 convert -density 300 "labelgen.pdf" -background white -flatten -alpha off "labelgen.png"
 
 TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
-cp "labelgen.pdf" ".log/$TIMESTAMP.pdf"
+mkdir -p .log
+cp "labelgen.png" ".log/$TIMESTAMP.png"
